a7c4e5472063a5cce0283d1981885ace678d2189
Antonio Cheong <acheong@student.dalat.org>
2023-03-04 19:52:55 +0800

    revChatGPT

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from twitchio.ext import commands
 from twitchio.message import Message
+from revChatGPT.V3 import Chatbot
 import os
 import re
 
@@ -20,6 +21,9 @@ class Bot(commands.Bot):
             initial_channels=["virtualharby"],
         )
         # self.bot = Chatbot(os.environ.get("email"), os.environ.get("password"))
+        self.chatbot = Chatbot(
+            api_key=os.environ.get("api_key"),
+        )
 
     async def event_ready(self):
         # We are logged in and ready to chat and use commands...
@@ -44,25 +48,22 @@ class Bot(commands.Bot):
         else:
             msg_details.author = message.author.name
 
-        # if msg_details.message.startswith("&"):
-        #     await self.handle_commands(context, msg_details)
+        if msg_details.message.startswith("&"):
+            await self.handle_commands(context, msg_details)
         if msg_details.author == "nightbot":
             await context.send(f"{msg_details.author}: {msg_details.message}")
         elif msg_details.isYouTube and msg_details.message.startswith("!"):
             await context.send(msg_details.message)
 
-    # async def handle_commands(
-    #     self, context: commands.Context, msg_details: CustomMsgDetails
-    # ):
-    #     # Split the message into command and arguments
-    #     command, *args = msg_details.message.split(" ")
-    #     if command == "&chatgpt":
-    #         full_response = ""
-    #         async for message in self.bot.ask(prompt=" ".join(args)):
-    #             full_response += message["choices"][0]["text"].replace("<|im_end|>", "")
-    #         if len(full_response) > 500:
-    #             for i in range(0, len(full_response), 500):
-    #                 await context.send(f"ChatGPT: {full_response[i:i + 500]}")
+    async def handle_commands(
+        self, context: commands.Context, msg_details: CustomMsgDetails
+    ):
+        # Split the message into command and arguments
+        command, *args = msg_details.message.split(" ")
+        if command == "&chatgpt":
+            prompt = " ".join(args)
+            response = self.chatbot.ask(prompt)
+            await context.send(response)
 
 
 bot = Bot()
